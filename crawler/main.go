8ebcd170ae22a819c443b64079b595992e78b53d
Jessica Paczuski <jessica@pimmr.com>
2017-02-23 12:17:49 +0100

    Push page.urls to RabbitMQ

--- crawler/main.go
+++ crawler/main.go
@@ -1,12 +1,16 @@
 package main
 
+// run RabbitMQ server in the console: $ rabbitmq-server
+
 import (
 	"fmt"
 	"net/http"
 	"time"
+  "log"
 
 	"github.com/PuerkitoBio/gocrawl"
 	"github.com/PuerkitoBio/goquery"
+  "github.com/streadway/amqp"
 )
 
 type Ext struct {
@@ -19,6 +23,14 @@ type Page struct {
 	Body string
 }
 
+func failOnError(err error, msg string) {
+  if err != nil {
+    log.Fatalf("%s: %s", msg, err)
+    panic(fmt.Sprintf("%s: %s", msg, err))
+  }
+}
+
+// crawl methods
 func (e *Ext) Visit(ctx *gocrawl.URLContext, res *http.Response, doc *goquery.Document) (interface{}, bool) {
 	fmt.Printf("Visit: %s\n", ctx.URL())
 	url := fmt.Sprintf("%s", ctx.URL())
@@ -39,9 +51,28 @@ func (e *Ext) Filter(ctx *gocrawl.URLContext, isVisited bool) bool {
 
 func main() {
 	pagechan := make(chan Page)
+
+  // connect to RabbitMQ
+  conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+  failOnError(err, "Failed to connect to RabbitMQ")
+  defer conn.Close()
+  ch, err := conn.Channel()
+  failOnError(err, "Failed to open a channel")
+  defer ch.Close()
+
+  q, err := ch.QueueDeclare(
+    "webpage", // name
+    false,   // durable
+    false,   // delete when unused
+    false,   // exclusive
+    false,   // no-wait
+    nil,     // arguments
+  )
+  failOnError(err, "Failed to declare a queue")
+
 	//crawl
 	ext := &Ext{&gocrawl.DefaultExtender{}, pagechan}
-	// Set custom options
+	// Set custom crawl options
 	opts := gocrawl.NewOptions(ext)
 	opts.CrawlDelay = 1 * time.Second
 	opts.LogFlags = gocrawl.LogError
@@ -51,7 +82,18 @@ func main() {
 	c := gocrawl.NewCrawlerWithOptions(opts)
 	go c.Run("http://0value.com")
 
-	for p := range pagechan {
-		fmt.Println("from channel: ", p.Url)
+	for page := range pagechan {
+    // push page to RabbitMQ
+    err = ch.Publish(
+      "",     // exchange
+      q.Name, // routing key
+      false,  // mandatory
+      false,  // immediate
+      amqp.Publishing {
+        ContentType: "text/plain",
+        Body:        []byte(page.Url),
+      })
+    log.Printf(" [x] Sent %s", page.Url)
+    failOnError(err, "Failed to publish a message")
 	}
 }
