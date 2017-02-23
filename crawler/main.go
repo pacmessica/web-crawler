b99c0fec46fa4d82fc458def2668e9b5668b8301
Jessica Paczuski <jessica@pimmr.com>
2017-02-23 12:23:49 +0100

    Fix line spacing

--- crawler/main.go
+++ crawler/main.go
@@ -3,24 +3,24 @@ package main
 // run RabbitMQ server in the console: $ rabbitmq-server
 
 import (
-	"fmt"
-	"net/http"
-	"time"
+  "fmt"
+  "net/http"
+  "time"
   "log"
 
-	"github.com/PuerkitoBio/gocrawl"
-	"github.com/PuerkitoBio/goquery"
+  "github.com/PuerkitoBio/gocrawl"
+  "github.com/PuerkitoBio/goquery"
   "github.com/streadway/amqp"
 )
 
 type Ext struct {
-	*gocrawl.DefaultExtender
-	PageChannel chan Page
+  *gocrawl.DefaultExtender
+  PageChannel chan Page
 }
 
 type Page struct {
-	Url string
-	Body string
+  Url string
+  Body string
 }
 
 func failOnError(err error, msg string) {
@@ -32,25 +32,25 @@ func failOnError(err error, msg string) {
 
 // crawl methods
 func (e *Ext) Visit(ctx *gocrawl.URLContext, res *http.Response, doc *goquery.Document) (interface{}, bool) {
-	fmt.Printf("Visit: %s\n", ctx.URL())
-	url := fmt.Sprintf("%s", ctx.URL())
-	body := fmt.Sprintf("%s", res.Body)
-	e.PageChannel <- Page{url, body}
-	return nil, true
+  fmt.Printf("Visit: %s\n", ctx.URL())
+  url := fmt.Sprintf("%s", ctx.URL())
+  body := fmt.Sprintf("%s", res.Body)
+  e.PageChannel <- Page{url, body}
+  return nil, true
 }
 
 func (e *Ext) Filter(ctx *gocrawl.URLContext, isVisited bool) bool {
-	if isVisited {
-		return false
-	}
-	if ctx.URL().Host == "github.com" || ctx.URL().Host == "golang.org" || ctx.URL().Host == "0value.com" {
-		return true
-	}
-	return false
+  if isVisited {
+    return false
+  }
+  if ctx.URL().Host == "github.com" || ctx.URL().Host == "golang.org" || ctx.URL().Host == "0value.com" {
+    return true
+  }
+  return false
 }
 
 func main() {
-	pagechan := make(chan Page)
+  pagechan := make(chan Page)
 
   // connect to RabbitMQ
   conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
@@ -70,19 +70,19 @@ func main() {
   )
   failOnError(err, "Failed to declare a queue")
 
-	//crawl
-	ext := &Ext{&gocrawl.DefaultExtender{}, pagechan}
-	// Set custom crawl options
-	opts := gocrawl.NewOptions(ext)
-	opts.CrawlDelay = 1 * time.Second
-	opts.LogFlags = gocrawl.LogError
-	opts.SameHostOnly = false
-	opts.MaxVisits = 100
+  //crawl
+  ext := &Ext{&gocrawl.DefaultExtender{}, pagechan}
+  // Set custom crawl options
+  opts := gocrawl.NewOptions(ext)
+  opts.CrawlDelay = 1 * time.Second
+  opts.LogFlags = gocrawl.LogError
+  opts.SameHostOnly = false
+  opts.MaxVisits = 100
 
-	c := gocrawl.NewCrawlerWithOptions(opts)
-	go c.Run("http://0value.com")
+  c := gocrawl.NewCrawlerWithOptions(opts)
+  go c.Run("http://0value.com")
 
-	for page := range pagechan {
+  for page := range pagechan {
     // push page to RabbitMQ
     err = ch.Publish(
       "",     // exchange
@@ -95,5 +95,5 @@ func main() {
       })
     log.Printf(" [x] Sent %s", page.Url)
     failOnError(err, "Failed to publish a message")
-	}
+  }
 }
